f202464bf8f8642ceac17d2c03feb34297c5f9a6
taligadoalex-blip <taligadoalex@gmail.com>
2026-09-09 19:07:31 -0300

    Add files via upload

--- NEVADAS_CALL_APP/ABRIR_NEVADAS.bat
+++ NEVADAS_CALL_APP/ABRIR_NEVADAS.bat
@@ -1,9 +1,12 @@
 @echo off
+setlocal
 cd /d "%~dp0"
-if exist "dist\NEVADAS CALL.exe" (
-  start "" "dist\NEVADAS CALL.exe"
-) else (
-  echo O aplicativo ainda nao foi criado.
-  echo Execute primeiro CRIAR_NEVADAS.bat
+if not exist "%~dp0dist\NEVADAS CALL Portable.exe" (
+  echo O executavel ainda nao foi criado.
+  echo Execute CRIAR_NEVADAS.bat primeiro.
+  echo.
   pause
+  exit /b 1
 )
+start "NEVADAS CALL" "%~dp0dist\NEVADAS CALL Portable.exe"
+endlocal
